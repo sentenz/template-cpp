e3fc8a0e82a623388acec45b8f9c5119b9859c06
sentenz <36011588+sentenz@users.noreply.github.com>
2025-10-07 15:00:56 +0200

    chore: modify resource, bootstrap, packages for CMake

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,6 +13,11 @@ source ./../scripts/shell/pkg.sh
 
 # Constant Variables
 
+readonly -A PIP_PACKAGES=(
+  ["gcovr"]="8.4"
+  ["conan"]="2.21.0"
+)
+
 readonly -A GO_PACKAGES=(
   ["go.mozilla.org/sops/cmd/sops"]="3.4.0"
 )
@@ -21,6 +26,12 @@ readonly -A GO_PACKAGES=(
 
 function setup() {
   local -i retval=0
+
+  pkg_pip_install_list PIP_PACKAGES
+  ((retval |= $?))
+
+  pkg_pip_clean
+  ((retval |= $?))
 
   pkg_go_install_list GO_PACKAGES
   ((retval |= $?))
